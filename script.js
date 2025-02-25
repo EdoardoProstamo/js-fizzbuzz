ed85bc82e7a55a9b7394136592d77896d7e72021
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-02-25 23:56:24 +0100

    feat: punto 2 completato

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // Scrivi un programma che stampi i numeri da 1 a 100
 for (let i=1; i<=100; i++) {
     console.log(i);
-}
 
 // Per i multipli di 3 stampi “Fizz” al posto del numero.
+if (i%3==0) {
+    console.log("Fizz");
+}
+}
 
 // Per i multipli di 5 stampi "Buzz" al posto del numero.
 
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
+// i=3; i<=100; i= i+3
