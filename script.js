abc76b97b95e77747f0115b31c1e1f005769e54e
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-02-26 00:03:52 +0100

    feat: inversione

--- script.js
+++ script.js
@@ -2,19 +2,18 @@
 for (let i=1; i<=100; i++) {
     console.log(i);
 
-// Per i multipli di 3 stampi “Fizz” al posto del numero.
-if (i%3==0) {
-    console.log("Fizz");
-} 
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
+if (i%3==0 && i%5==0) {
+    console.log("FizzBuzz");
+}
 // Per i multipli di 5 stampi "Buzz" al posto del numero.
 else if (i%5==0) {
     console.log("Buzz");
 }
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
-else if (i%3==0 && i%5==0) {
-    console.log("FizzBuzz");
-}
-    
+// Per i multipli di 3 stampi “Fizz” al posto del numero.
+else if (i%3==0) {
+    console.log("Fizz");
+} 
 }
 
 
