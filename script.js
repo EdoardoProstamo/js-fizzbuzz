055eb00e49b500b05e87cb74ddad60caa6506061
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-02-25 23:58:28 +0100

    feat: punto 3 compketato

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ for (let i=1; i<=100; i++) {
 // Per i multipli di 3 stampi “Fizz” al posto del numero.
 if (i%3==0) {
     console.log("Fizz");
+} 
+// Per i multipli di 5 stampi "Buzz" al posto del numero.
+else if (i%5==0) {
+    console.log("Buzz");
 }
+    
 }
 
-// Per i multipli di 5 stampi "Buzz" al posto del numero.
 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
-// i=3; i<=100; i= i+3
+
