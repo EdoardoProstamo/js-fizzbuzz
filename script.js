d2a52d4b7dce6a5cdacb61545e3e42682e711a1c
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-02-26 10:14:17 +0100

    fix

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Scrivi un programma che stampi i numeri da 1 a 100
 for (let i=1; i<=100; i++) {
-    console.log(i);
 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
 if (i%3==0 && i%5==0) {
@@ -13,7 +12,9 @@ else if (i%5==0) {
 // Per i multipli di 3 stampi “Fizz” al posto del numero.
 else if (i%3==0) {
     console.log("Fizz");
-} 
+} else {
+    console.log(i);
+}
 }
 
 
